892f00c4ca4d4bf276cdc5425e66b3d0fa64e205
itolkent <itol.kent@gmail.com>
2025-12-01 02:51:28 +0000

    Addding Dynamic form Text

--- src/CreateJob.js
+++ src/CreateJob.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 
 const JobCounter = () => {
     let [jobCount, setJobCounter] = useState(0)
+    const [inputVal, setInputValue] = useState("")
+    const [txt, setText] = useState("")
 
     const handleAddEvent = () => {
         setJobCounter(jobCount + 1)
@@ -16,6 +18,15 @@ const JobCounter = () => {
         setJobCounter(0)
         console.log('Job ' + jobCount)
     }
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value)
+        setText(event.target.value);
+
+    };
+    const handleClear = () => {
+        setInputValue("")
+        setText("");
+    };
 
     return (
         <div>
@@ -24,6 +35,23 @@ const JobCounter = () => {
             <button id='btnCtr' onClick={handleAddEvent}>Add Job</button>
             <button id='btnCtr' onClick={handleRemoveEvent}>Remove Job</button>
             <button id='btnCtr' onClick={handleResetEvent}>Reset</button>
+            <div>
+                <h1>Dynamic Form</h1>
+                <input
+                    type="text"
+                    value={txt}
+                    onChange={handleInputChange}
+                    placeholder="Type something..."
+                />
+                <button id='btnCtr' onClick={handleClear}>Clear</button>
+                <div>
+                    <h2>Current Input:</h2>
+                    <div className="input-container">
+
+                        <p>{inputVal}</p>
+                    </div>
+                </div>
+            </div>
         </div>
 
     );
